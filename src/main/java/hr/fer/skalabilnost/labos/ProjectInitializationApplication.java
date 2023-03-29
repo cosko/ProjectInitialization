279a099fad5acca82db663b0105354bf7e0bad99
Josip Frkin <10chos10@gmail.com>
2023-03-29 16:43:26 +0200

    init

---
+++ src/main/java/hr/fer/skalabilnost/labos/ProjectInitializationApplication.java
@@ -0,0 +1,13 @@
+package hr.fer.skalabilnost.labos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectInitializationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectInitializationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/fer/skalabilnost/labos/ProjectInitializationApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.fer.skalabilnost.labos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectInitializationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
